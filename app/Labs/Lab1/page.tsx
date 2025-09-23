e36489bb72faf41a228d788de3f9584f2cb474f2
PABPAT <poornaabhijith@gmail.com>
2025-09-23 01:14:41 -0400

    Second Commit

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -72,7 +72,7 @@ tag to tell browsers to render the gaps.
                 <ul id="wd-my-books">
                     <li>Dune</li>
                     <li>Lord of the Rings</li>
-                    <li>Ender's Game</li>
+                    <li>Ender&apos;s Game</li>
                     <li>Red Mars</li>
                     <li>The Forever War</li>
                 </ul>
@@ -81,7 +81,7 @@ tag to tell browsers to render the gaps.
                     <li>Alchemist</li>
                     <li>Shiva Trilogy</li>
                     <li>Chanakya Neeti</li>
-                    <li>Midnight's Children</li>
+                    <li>Midnight&apos;s Children</li>
                 </ul>
             </div>
             <div id="wd-tables">
@@ -154,19 +154,20 @@ tag to tell browsers to render the gaps.
                     It is a long established fact that a reader will be distracted by the readable 
                     content of a page when looking at its layout. The point of using Lorem Ipsum is 
                     that it has a more-or-less normal distribution of letters, as opposed to using 
-                    'Content here, content here', making it look like readable English. Many desktop 
+                    &apos;Content here, content here&apos;, making it look like readable English. Many desktop 
                     publishing packages and web page editors now use Lorem Ipsum as their default 
-                    model text, and a search for 'lorem ipsum' will uncover many web sites still in 
+                    model text, and a search for &apos;lorem ipsum&apos; will uncover many web sites still in 
                     their infancy. Various versions have evolved over the years, sometimes by accident, 
                     sometimes on purpose (injected humour and the like).
                     </textarea>
                     <h5 id="wd-buttons">Buttons</h5>
                     <button 
                         type="button"
-                        onClick = {() => "alert('Life is Good!')"} //button not working
+                        onClick={() => alert('Life is Good!')}
                         id="wd-all-good">
                         Hello World!
                     </button>
+
                     <h5 id="wd-radio-buttons">Radio buttons</h5>
 
                     <label>Favorite movie genre:</label><br />
