3918312fa9f4b0f576c67f3bf914b952c29f289a
Poorna Abhijth <poornaabhijith@gmail.com>
2025-10-22 22:45:07 -0400

    a3 JavaScript

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -61,7 +61,6 @@ export default function Lab3() {
       <Spreader />
       <Destructing />
       <FunctionDestructing />
-      {/*<Math />*/}
       <DestructingImports />
       <Classes />
       <Styles />
