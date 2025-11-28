065ab895ad024dab079e7386bdc71ed2e2c68cac
Poorna Abhijth <poornaabhijith@gmail.com>
2025-11-28 00:32:37 -0500

    Fixed currentUser issue

--- app/(kambaz)/Account/Navigation.tsx
+++ app/(kambaz)/Account/Navigation.tsx
@@ -11,7 +11,7 @@ export default function AccountNavigation() {
  return (
    <Nav variant="pills">
      <NavItem>
-     {currentUser && currentUser.role === "ADMIN" && (
+     {currentUser && currentUser['role'] === "ADMIN" && (
          <NavLink as={Link} href={`/Account/Users`} active={pathname.endsWith('Users')}> Users </NavLink>)}
      </NavItem>
      {links.map((link) => (
